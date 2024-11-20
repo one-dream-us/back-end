bece7b203640d186f0c4dcb1520caa06e27fe574
jujaeyeong <jjy5230@gmail.com>
2024-11-21 00:29:34 +0900

    [DEV-17] feat: cors 설정 추가 - 허가 URL 추후 수정 필요

--- src/main/java/com/onedreamus/project/global/config/security/SecurityConfig.java
+++ src/main/java/com/onedreamus/project/global/config/security/SecurityConfig.java
@@ -3,6 +3,10 @@ package com.onedreamus.project.global.config.security;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.onedreamus.project.global.config.jwt.JWTFilter;
 import com.onedreamus.project.global.config.jwt.JWTUtil;
+import jakarta.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,6 +19,8 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -37,6 +43,26 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        // cors 설정
+        //TODO: 추후 cors 허가 url 수정 필요 - 현재는 임시
+        http
+            .cors((cors) -> cors.configurationSource(new CorsConfigurationSource() {
+                @Override
+                public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+
+                    CorsConfiguration corsConfiguration = new CorsConfiguration();
+                    corsConfiguration.setAllowedOrigins(List.of("http://localhost:3000"));
+                    corsConfiguration.setAllowedMethods(List.of("*"));
+                    corsConfiguration.setAllowCredentials(true);
+                    corsConfiguration.setAllowedHeaders(Collections.singletonList("*"));
+                    corsConfiguration.setMaxAge(360L);
+
+                    corsConfiguration.setExposedHeaders(Collections.singletonList("Authorization"));
+
+                    return corsConfiguration;
+                }
+            }));
 
         http
             .csrf(AbstractHttpConfigurer::disable) // csrf disable
