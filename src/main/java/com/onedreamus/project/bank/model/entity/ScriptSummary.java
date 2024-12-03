83dc52e45e036ae629c916a924b3f9bf15223f0b
juhwano <juhwan.swe@gmail.com>
2024-12-03 09:24:38 +0900

    [DEV-22] feat: 콘텐츠 리스트 API 요약문 정보 추가

--- src/main/java/com/onedreamus/project/bank/model/entity/ScriptSummary.java
+++ src/main/java/com/onedreamus/project/bank/model/entity/ScriptSummary.java
@@ -9,15 +9,20 @@ import lombok.*;
 @Getter
 @Setter
 @Builder
+@Table(name = "script_summary")
 public class ScriptSummary {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    private Long id;
 
-    private String summaryText;
+	@Column(name = "summary_text")
+	private String summaryText;
 
-    @JoinColumn
-    @OneToOne(fetch = FetchType.LAZY)
-    private Content content;
+	@Column(name = "content_id")
+	private Long contentId;
+
+	@OneToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "content_id", insertable = false, updatable = false)
+	private Content content;
 }
