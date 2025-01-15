a64c6a6b15a81d6d679907302ddf5eafd2ac496b
jujaeyeong <jjy5230@gmail.com>
2025-01-15 22:34:11 +0900

    [DEV-232] fix: 뉴스 콘텐츠 제공 시 원본 기사 link 제공

--- src/main/java/com/onedreamus/project/thisismoney/repository/DictionarySentenceRepository.java
+++ src/main/java/com/onedreamus/project/thisismoney/repository/DictionarySentenceRepository.java
@@ -4,6 +4,8 @@ import com.onedreamus.project.thisismoney.model.entity.Dictionary;
 import com.onedreamus.project.thisismoney.model.entity.DictionarySentence;
 import com.onedreamus.project.thisismoney.model.entity.Sentence;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -14,6 +16,7 @@ public interface DictionarySentenceRepository extends JpaRepository<DictionarySe
 
     Optional<DictionarySentence> findBySentence(Sentence sentence);
 
-    List<Dictionary> findDictionaryBySentenceIn(List<Sentence> sentences);
+    @Query("SELECT ds.dictionary FROM DictionarySentence ds WHERE ds.sentence IN :sentences")
+    List<Dictionary> findDictionaryBySentenceIn(@Param("sentences") List<Sentence> sentences);
 
 }
