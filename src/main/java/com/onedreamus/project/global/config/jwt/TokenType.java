f2784bd90eadd2006870ca4f05dbb1ed75941c9e
jujaeyeong <jjy5230@gmail.com>
2025-02-10 00:56:26 +0900

    fix: 토큰 만료시간 원복

--- src/main/java/com/onedreamus/project/global/config/jwt/TokenType.java
+++ src/main/java/com/onedreamus/project/global/config/jwt/TokenType.java
@@ -10,8 +10,8 @@ import java.util.List;
 @AllArgsConstructor
 public enum TokenType {
 
-    //    REFRESH_TOKEN("REFRESH-TOKEN", 60 * 24 * 5 * 60 * 1000L), // 5일
-    REFRESH_TOKEN("REFRESH-TOKEN", 20 * 60 * 1000L),
+    REFRESH_TOKEN("REFRESH-TOKEN", 60 * 24 * 5 * 60 * 1000L), // 5일
+    //    REFRESH_TOKEN("REFRESH-TOKEN", 20 * 60 * 1000L),
     ACCESS_TOKEN("ACCESS-TOKEN", 10 * 60 * 1000L), // 10분
     VERIFY_TOKEN("VERIFY-TOKEN", 3 * 60 * 1000L);
 
