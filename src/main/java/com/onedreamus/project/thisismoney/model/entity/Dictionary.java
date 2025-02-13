fbb3dc17da80f4d2f59e14eb05ec10c7b3fd106d
Jae Young <jjy5230@gmail.com>
2025-02-13 16:13:04 +0900

    [DEV-254] feat: 뉴스 등록 시 용어 정의에 하이라이트 마킹 추가

--- src/main/java/com/onedreamus/project/thisismoney/model/entity/Dictionary.java
+++ src/main/java/com/onedreamus/project/thisismoney/model/entity/Dictionary.java
@@ -5,15 +5,14 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import jakarta.websocket.server.ServerEndpoint;
+import lombok.*;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Builder
+@Setter
 @Table(name="dictionary")
 @Entity(name="Dictionary")
 public class Dictionary {
