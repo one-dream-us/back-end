f2743065d7138eafcd96997f71d755756cad1124
jujaeyeong <jjy5230@gmail.com>
2025-02-16 01:29:11 +0900

    [DEV-254] refactor: 뉴스 업로드 시 데이터를 @RequestPart로 처리하도록 수정

--- src/main/java/com/onedreamus/project/thisismoney/model/dto/NewsRequest.java
+++ src/main/java/com/onedreamus/project/thisismoney/model/dto/NewsRequest.java
@@ -20,17 +20,9 @@ public class NewsRequest {
     @NotBlank(message = "뉴스 제목은 필수 값 입니다.")
     private String title; // 뉴스 제목
 
-
-    private MultipartFile thumbnailImage; // 썸네일 URL
-
     @NotBlank(message = "원본 링크는 필수 값 입니다.")
     private String originalLink; // 기사 원본 링크
 
     @NotBlank(message = "에이전시 값은 필수 값 입니다.")
     private String newsAgency; // 뉴스 업로드한 에이전시
-
-    @Valid
-    @NotEmpty(message = "용어, 문장 값은 필수 입니다.")
-    private List<DictionarySentenceRequest> dictionarySentenceList;
-
 }
